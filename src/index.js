2a86be2b9db10adc6d7f58ae20cd9124041d0e5d
Nolankicks <nolandimensions@gmail.com>
2024-03-03 16:06:46 -0500

    Updates

--- src/index.js
+++ src/index.js
@@ -10,29 +10,69 @@ const client = new Client({
     ],
 });
 
-client.on('messageCreate', (message) => {
-    if (message.has === '/paper')
-    {
-        message.reply('sizzors')
-        message.reply('you suck bitch, I win')
-    }
-});
-client.on('messageCreate', (message) => {
-    if (message.content === '/scissors')
-    {
-        message.reply('rock')
-        message.reply('you suck bitch, I win')
-    }
-});
-client.on('messageCreate', (message) => {
-    if (message.content === '/rock')
-    {
-        message.reply('paper')
-        message.reply('you suck bitch, I win')
-    }
-});
+
+
+
 
 client.login(process.env.TOKEN);
+
+function getMove() {
+    const replies = ['paper', 'rock', 'scissors' ];
+    const random = Math.floor(Math.random() * 3)
+    
+    return replies[random]
+}
+
+client.on('messageCreate', (message) =>
+{
+    if (message.content === '/paper')
+    {
+        if (getMove() == 'paper')
+        {
+            message.reply('I choose paper! It\'s a tie!');
+        }
+
+        if (getMove() == 'rock')
+        {
+            message.reply('I choose rock! You win!');
+        }
+
+        if (getMove() == 'scissors')
+        {
+            message.reply('I choose scissors! You lose!');
+        }
+    }
+
+})
+
+client.on('messageCreate', (message) =>
+{
+    if (message.content === '/sizzors')
+    {
+        if (getMove() == 'sizzors')
+        {
+            message.reply('I choose sizzors! It\'s a tie!');
+        }
+
+        if (getMove() == 'rock')
+        {
+            message.reply('I choose rock! I lose!');
+        }
+
+        if (getMove() == 'paper')
+        {
+            message.reply('I choose paper! You win!');
+        }
+    }
+
+})
+
+
+
+console.log(getMove());
+
+
+
 
 
 
