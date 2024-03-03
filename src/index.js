68685fab2455a9bf0555b50a1e51a5a38c511d6f
Nolankicks <nolandimensions@gmail.com>
2024-03-03 16:42:19 -0500

    Refactor game logic and handle bot messages

--- src/index.js
+++ src/index.js
@@ -25,21 +25,26 @@ function getMove() {
 
 client.on('messageCreate', (message) =>
 {
+    if (message.author.bot) return;
+
     if (message.content === '/paper')
     {
-        if (getMove() == 'paper')
+        if (getMove() === 'paper')
         {
             message.reply('I choose paper! It\'s a tie!');
+            getMove();
         }
 
-        if (getMove() == 'rock')
+        if (getMove() === 'rock')
         {
             message.reply('I choose rock! You win!');
+            getMove();
         }
 
-        if (getMove() == 'scissors')
+        if (getMove() === 'scissors')
         {
             message.reply('I choose scissors! You lose!');
+            getMove();
         }
     }
 
@@ -47,21 +52,26 @@ client.on('messageCreate', (message) =>
 
 client.on('messageCreate', (message) =>
 {
+    if (message.author.bot) return;
+
     if (message.content === '/sizzors')
     {
-        if (getMove() == 'sizzors')
+        if (getMove() === 'sizzors')
         {
             message.reply('I choose sizzors! It\'s a tie!');
+            getMove();
         }
 
-        if (getMove() == 'rock')
+        if (getMove() === 'rock')
         {
             message.reply('I choose rock! I lose!');
+            getMove();
         }
 
-        if (getMove() == 'paper')
+        if (getMove() === 'paper')
         {
             message.reply('I choose paper! You win!');
+            getMove();
         }
     }
 
@@ -69,21 +79,26 @@ client.on('messageCreate', (message) =>
 
 client.on('messageCreate', (message) =>
 {
+    if (message.author.bot) return;
+
     if (message.content === '/rock')
     {
-        if (getMove() == 'rock')
+        if (getMove() === 'rock')
         {
             message.reply('I choose rock! It\'s a tie!');
+            getMove();
         }
 
-        if (getMove() == 'sizzors')
+        if (getMove() === 'sizzors')
         {
             message.reply('I choose sizzors! You win!');
+            getMove();
         }
 
-        if (getMove() == 'paper')
+        if (getMove() === 'paper')
         {
             message.reply('I choose paper! You lose!');
+            getMove();
         }
     }
 })
