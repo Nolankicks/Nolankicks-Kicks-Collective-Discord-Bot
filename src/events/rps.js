da99bfeaaf38efd97e78582f5a7ef1781ea43d94
Nolankicks <nolandimensions@gmail.com>
2024-03-05 18:52:30 -0500

    testing

--- src/events/rps.js
+++ src/events/rps.js
@@ -1,4 +1,4 @@
-const { Client  } = require('discord.js');
+const { Client, ButtonBuilder, ButtonStyle, ActionRowBuilder  } = require('discord.js');
 const Coin = require('./kicksCoinSchema.js');
 const { EmbedBuilder } = require('discord.js');
 module.exports["RPS"] = async (Client) => 
@@ -16,7 +16,6 @@ module.exports["RPS"] = async (Client) =>
     client.on('interactionCreate', async (interaction) =>
     {
         //Checks
-        if (!interaction.isChatInputCommand()) return;
         //Find the users coins
         const query = {
             userID: interaction.user.id,
@@ -32,7 +31,23 @@ module.exports["RPS"] = async (Client) =>
             interaction.reply('You can\'t bet a negative amount of coins!');
             return;
         }
-
+        const buttons = [
+            {
+                label: 'Rock',
+                style: 'PRIMARY',
+                customId: 'rock',
+            },
+            {
+                label: 'Paper',
+                style: 'PRIMARY',
+                customId: 'paper',
+            },
+            {
+                label: 'Scissors',
+                style: 'PRIMARY',
+                customId: 'scissors',
+            }
+        ]
         try {
             //Find the users coins
             const messageCoin = await Coin.findOne(query);
@@ -46,25 +61,25 @@ module.exports["RPS"] = async (Client) =>
             //If the command is rps
             if (interaction.commandName === 'rps') 
             {
-                //Define the options
-                const rock = interaction.options.getBoolean('rock');
-                const paper = interaction.options.getBoolean('paper');
-                const scissors = interaction.options.getBoolean('scissors');
-                //Makes sure the user can't choose multiple options
-                if (rock && paper && scissors) {
-                    interaction.reply('You can only choose one option at a time!');
-                    return;
-                }
-                //Makes sure the user chooses an option
-                if (!rock && !paper && !scissors) {
-                    interaction.reply('You need to choose an option!');
-                    return;
-                }
-                //Get the bot move
-                const botMove = getMove();
-                //If the player's move is rock
-                if (rock)
+                
+                const row = new ActionRowBuilder();
+
+               try {
+                buttons.forEach(async (button) => {
+                    row.components.push(
+                        new ButtonBuilder().setCustomId(button.customId).setLabel(button.label).setStyle(ButtonStyle.Primary),
+                    )
+                });
+               } catch (error) {
+                console.log(error);
+               }
+                interaction.reply({ content: 'Choose your move!', components: [row] });
+            }
+            const botMove = getMove();
+            console.log(interaction.customId);
+            if (interaction.customId === 'rock')
                 {
+                    console.log(interaction.customId);
                     if (botMove === 'rock') {
                         interaction.reply('I chose rock, it\'s a tie!');
                     } else if (botMove === 'paper') {
@@ -85,7 +100,7 @@ module.exports["RPS"] = async (Client) =>
                     return;
                 }
                 //If the player's move is paper
-                if (paper)
+                if (interaction.customId === 'paper')
                 {
                     if (botMove === 'rock') {
                         const rockWin = new EmbedBuilder().setTitle('I chose rock, you win!').setFooter({ text: 'Congratulations!' });
@@ -107,7 +122,7 @@ module.exports["RPS"] = async (Client) =>
                     return;
                 }
                 //If the player's move is scissors
-                if (scissors)
+                if (interaction.customId === 'scissors')
                 {
                     if (botMove === 'rock') {
                         const rockLoss = new EmbedBuilder().setTitle('I chose rock, I win!').setFooter({ text: 'Better luck next time!' });
@@ -128,12 +143,6 @@ module.exports["RPS"] = async (Client) =>
                     }
                     return;
                 }
-                //If the user chooses multiple options
-                if (rock && paper || rock && scissors || paper && scissors || rock && paper && scissors) {
-                    interaction.reply('You can only choose one option at a time!');
-                    return;
-                }
-            }
             if (messageCoin.coins === null || messageCoin.coins === undefined)
             {
                 const newCoin = new Coin({
@@ -149,5 +158,5 @@ module.exports["RPS"] = async (Client) =>
         //Catch and log the error
         console.log(error);     
         }
-    }) 
+    });
 }
